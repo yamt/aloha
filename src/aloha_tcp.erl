1884d1160789f0203b50a02ba80b168730666082
YAMAMOTO Takashi <yamt@mwd.biglobe.ne.jp>
2013-08-21 18:05:40 +0900

    tweak logging to output window info

--- src/aloha_tcp.erl
+++ src/aloha_tcp.erl
@@ -87,7 +87,7 @@ new_connection(Tcp, Stack, Key, Opts, Listener) ->
     NewPid.
 
 handle_tcp(#tcp{checksum=good} = Tcp, Stack, Data, Opts) ->
-    lager:info("TCP RECV ~s", [tcp_summary(Tcp, Data)]),
+    lager:info("RECV ~s", [tcp_summary(Tcp, Data)]),
     [Ip|_] = Stack,
     Key = {Ip#ip.src, Ip#ip.dst, Tcp#tcp.src_port, Tcp#tcp.dst_port},
     Pid = try ets:lookup_element(aloha_tcp_conn, Key, 2)
@@ -118,7 +118,7 @@ reply_rst(Tcp, Stack, Data, Backend) ->
 
 send_packet(Pkt, Backend) ->
     [_, _, Tcp, Data] = Pkt,
-    lager:info("TCP SEND ~s", [tcp_summary(Tcp, Data)]),
+    lager:info("SEND ~s", [tcp_summary(Tcp, Data)]),
     BinPkt = aloha_packet:encode_packet(Pkt),
 %   [aloha_nic:send_packet(BinPkt, Backend) ||
 %    random:uniform() > 0.5],  % drop packets for testing
@@ -130,12 +130,13 @@ flag(0, _) ->
     ".".
 
 tcp_summary(#tcp{dst_port = Dst, src_port = Src, syn = Syn, ack = Ack,
-                 rst = Rst, psh = Psh, fin = Fin,
+                 rst = Rst, psh = Psh, fin = Fin, window = Win,
                  seqno = Seq, ackno = Ackno} = Tcp,
             Data) ->
-    [integer_to_list(Src), " -> ", integer_to_list(Dst), " ",
+    [integer_to_list(Src), "->", integer_to_list(Dst), " ",
      flag(Syn, "S"), flag(Ack, "A"), flag(Rst, "R"), flag(Psh, "P"),
      flag(Fin, "F"), " ",
      "seq ", integer_to_list(Seq),
      " len ", integer_to_list(aloha_tcp_conn:seg_len(Tcp, Data)),
-     " ack ", integer_to_list(Ackno)].
+     " ack ", integer_to_list(Ackno),
+     " win ", integer_to_list(Win)].
